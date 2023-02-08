d8dc14243c5568939be2a75a696e7f502a604538
unknown <rownakhayat@gmail.com>
2023-02-08 17:33:16 +0600

    Component add

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import Activities from './Components/Activities/Activities';
 
 function App() {
   return (
     <div className="App">
-      
+      <Activities></Activities>
     </div>
   );
 }
